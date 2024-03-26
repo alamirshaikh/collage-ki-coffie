e28a6f51d7833c971f1fb78f7aa3e4a31c3d919e
alamirshaikh <amirshaikhsystem@gmail.com>
2024-03-26 22:37:05 +0530

    first commit

---
+++ collage-ki-coffie/collage-ki-coffie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace collage_ki_coffie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
